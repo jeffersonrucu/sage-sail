4f90683387f90f41d42ddf5860039a1ef94f6aa8
Jefferson Oliveira <jefferoidtex7@gmail.com>
2025-11-07 23:25:31 -0300

    chore(sail): update commands, vendor path, and labels to sage-sail

--- src/Console/AddCommand.php
+++ src/Console/AddCommand.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 use Laravel\Sail\Console\Concerns\InteractsWithDockerComposeServices;
 use Symfony\Component\Console\Attribute\AsCommand;
 
-#[AsCommand(name: 'sail:add')]
+#[AsCommand(name: 'sage-sail:add')]
 class AddCommand extends Command
 {
     use InteractsWithDockerComposeServices;
@@ -16,7 +16,7 @@ class AddCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'sail:add
+    protected $signature = 'sage-sail:add
         {services? : The services that should be added}
     ';
 
@@ -25,7 +25,7 @@ class AddCommand extends Command
      *
      * @var string
      */
-    protected $description = 'Add a service to an existing Sail installation';
+    protected $description = 'Add a service to an existing Sage Sail installation';
 
     /**
      * Execute the console command.
@@ -55,6 +55,6 @@ class AddCommand extends Command
         $this->prepareInstallation($services);
 
         $this->output->writeln('');
-        $this->components->info('Additional Sail services installed successfully.');
+        $this->components->info('Additional Sage Sail services installed successfully.');
     }
 }
